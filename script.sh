6cb7229bf3fd3973f7d8ec7f0c1afccb673a482c
lbiondos <lbiondos@umich.edu>
2023-04-06 19:18:14 -0400

    Your commit message

--- script.sh
+++ script.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 # Check if the script is being run from the correct directory
-if [[ ! -f "$1" ]]; then
+if [[ ! -f "source/$1" ]]; then
   echo "Usage: $0 <latex-file>"
   exit 1
 fi
 
+# Set the output directory for the generated HTML file
+OUTPUT_DIR="build"
+
 # Convert the LaTeX file to HTML using Pandoc
-pandoc -s -f latex -t html -o "${1%.tex}.html" "$1"
+pandoc -s -f latex -t html -o "${OUTPUT_DIR}/${1%.tex}.html" "source/$1"
 
 # Check if the conversion was successful
 if [[ $? -ne 0 ]]; then
@@ -16,4 +19,5 @@ if [[ $? -ne 0 ]]; then
 fi
 
 # Print a success message
-echo "Successfully converted $1 to HTML"
+echo "Successfully converted $1 to HTML in the ${OUTPUT_DIR} directory"
+
